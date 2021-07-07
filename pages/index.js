8d6eb5bc1c69c3eccdecdc2d71de9ce4f6579472
Sam Goddard <hello@samgoddard.co.uk>
2021-07-07 12:22:59 +0100

    lint tidyup

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from '@/components/header'
 import Footer from '@/components/footer'
 import Container from '@/components/container'
 import FancyLink from '@/components/fancyLink'
-import { fade } from "@/helpers/transitions"
+import { fade } from '@/helpers/transitions'
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { NextSeo } from 'next-seo'
 

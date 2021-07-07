9a8115edb13fafd6258d30a803f406447c9788ac
Sam Goddard <hello@samgoddard.co.uk>
2021-07-07 11:09:04 +0100

    moved loco container to individual page layer

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,19 @@
 import '@/styles/main.css'
-import { useRef } from 'react'
 import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
-  const containerRef = useRef(null)
 
   return (
     <>
       <DefaultSeo {...SEO} />
 
-      <LocomotiveScrollProvider
-        options={{
-          smooth: true,
-          lerp: 0.05
-        }}
-        containerRef={containerRef}
-        watch={[]}
-      >
-        <div data-scroll-container ref={containerRef} id="scroll-container">
-          <AnimatePresence exitBeforeEnter>
-            <Component {...pageProps} key={router.asPath} />
-          </AnimatePresence>
-        </div>
-      </LocomotiveScrollProvider>
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
     </>
   )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,70 +1,81 @@
+import { useRef } from 'react'
 import Layout from '@/components/layout'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import Container from '@/components/container'
 import FancyLink from '@/components/fancyLink'
 import { fade } from "@/helpers/transitions"
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { NextSeo } from 'next-seo'
 
 export default function About() {
+  const containerRef = useRef(null)
   return (
     <Layout>
       <NextSeo title="About" />
 
-      <div data-scroll-section>
-        <Header />
+      <LocomotiveScrollProvider
+        options={{ smooth: true, lerp: 0.05 }}
+        containerRef={containerRef}
+        watch={[]}
+      >
+        <div data-scroll-container ref={containerRef} id="scroll-container">
+          <div data-scroll-section>
+            <Header />
 
-        <LazyMotion features={domAnimation}>
-          <m.div
-            initial="initial"
-            animate="enter"
-            exit="exit"
-            className="mb-12 md:mb-16 xl:mb-24 pt-24 md:pt-20"
-          >
-            <Container>
-              <m.div variants={fade}>
-                <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl mb-4">About Page</h1>
-                <div className="content max-w-3xl mb-4">
-                <h2>Some example content</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+            <LazyMotion features={domAnimation}>
+              <m.div
+                initial="initial"
+                animate="enter"
+                exit="exit"
+                className="mb-12 md:mb-16 xl:mb-24 pt-24 md:pt-20"
+              >
+                <Container>
+                  <m.div variants={fade}>
+                    <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl mb-4">About Page</h1>
+                    <div className="content max-w-3xl mb-4">
+                    <h2>Some example content</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
-                  <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                  <h2>Some example content</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                      <h2>Some example content</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
-                  <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                  <h2>Some example content</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                      <h2>Some example content</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
-                  <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                  <h2>Some example content</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                      <h2>Some example content</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
-                  <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                  <h2>Some example content</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                      <h2>Some example content</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
-                  <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                  <h2>Some example content</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+                      <h2>Some example content</h2>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
 
-                  <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                
-                <FancyLink destination="/" a11yText="Navigate to the home page" label="Home Page" />
+                      <p>Velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    
+                    <FancyLink destination="/" a11yText="Navigate to the home page" label="Home Page" />
+                  </m.div>
+                </Container>
               </m.div>
-            </Container>
-          </m.div>
-        </LazyMotion>
+            </LazyMotion>
 
-        <Footer />
-      </div>
+            <Footer />
+          </div>
+        </div>
+      </LocomotiveScrollProvider>
     </Layout>
   )
 }

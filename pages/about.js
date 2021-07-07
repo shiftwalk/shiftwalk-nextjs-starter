9129195df4258b35b325e674562f368d56c21519
Sam Goddard <hello@samgoddard.co.uk>
2021-07-07 12:23:28 +0100

    lint tidyup

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import Header from '@/components/header'
 import Footer from '@/components/footer'
 import Container from '@/components/container'
 import FancyLink from '@/components/fancyLink'
-import { fade } from "@/helpers/transitions"
+import { fade } from '@/helpers/transitions'
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { NextSeo } from 'next-seo'
